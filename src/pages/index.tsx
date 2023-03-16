825c777ce853dad73bef537b9ad6665d4ec3ed48
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-16 14:46:52 +0100

    refactor: page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,20 @@ export default function IndexPage() {
   return (
     <div>
       <AppScheme>
-        <h1>Francisco Fetapi</h1>
+        <h2>Visualização Rápida</h2>
+        <ul>
+          <li>
+            gráfico indicativo de quanto ainda há disponível de faturamento para
+            emitir uma Nota Fiscal sem que haja desenquadramento como MEI
+          </li>
+          <li>gráficos com o valor de NF gerado, mês a mês</li>
+          <li>gráficos com o valor de despesas mês a mês</li>
+          <li>
+            gráficos com balanço simples, mostrando receitas - despesas mês a
+            mês
+          </li>
+          <li>gráficos com as despesas por categorias</li>
+        </ul>
       </AppScheme>
     </div>
   );
