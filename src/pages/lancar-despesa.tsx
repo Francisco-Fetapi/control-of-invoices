ef3309b296395f1100cae515a8091333ef90e763
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-17 17:54:55 +0100

    feat: template lancar-despesa page created

---
+++ src/pages/lancar-despesa.tsx
@@ -0,0 +1,13 @@
+import AppScheme from "layouts/AppScheme";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <AppScheme>
+        <h2>Lançar Despesa</h2>
+
+        {/* <FormLaunchInvoice /> */}
+      </AppScheme>
+    </div>
+  );
+}
