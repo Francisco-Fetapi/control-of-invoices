0e1f3d11fff099384a633a3102f5fd41d54528c5
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-17 18:25:12 +0100

    feat: invoice table created

---
+++ src/components/InvoicesTable.tsx
@@ -0,0 +1,141 @@
+import { useState } from "react";
+import {
+  createStyles,
+  Switch,
+  Table,
+  Checkbox,
+  Group,
+  Text,
+  ActionIcon,
+} from "@mantine/core";
+import { TableMenuRow } from "./TableMenuRow";
+import { ExpenseCategory } from "pages/categoria-despesas";
+import getShortText from "helpers/getShortText";
+import { IconToggleRight, IconToggleLeft } from "@tabler/icons";
+
+const useStyles = createStyles((theme) => ({
+  rowSelected: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
+        : theme.colors[theme.primaryColor][0],
+  },
+}));
+
+export interface Invoice {
+  id: string;
+  value: string;
+  number: number;
+  description: string;
+  accrualMonth: Date;
+  receiptDate: Date;
+  corporationName: string;
+}
+
+export interface InvoicesTableProps {
+  data: Invoice[];
+}
+
+export function InvoicesTable({ data }: InvoicesTableProps) {
+  const { classes, cx } = useStyles();
+  const [selection, setSelection] = useState(["1"]);
+  const toggleRow = (id: string) =>
+    setSelection((current) =>
+      current.includes(id)
+        ? current.filter((item) => item !== id)
+        : [...current, id]
+    );
+  const toggleAll = () =>
+    setSelection((current) =>
+      current.length === data.length ? [] : data.map((item) => item.id)
+    );
+
+  const rows = data.map((item, key) => {
+    return (
+      <TableRow
+        selection={selection}
+        item={item}
+        key={item.id}
+        toggleRow={toggleRow}
+      />
+    );
+  });
+
+  return (
+    <Table verticalSpacing="sm">
+      <thead>
+        <tr>
+          <th style={{ width: 40 }}>
+            <Checkbox
+              onChange={toggleAll}
+              checked={selection.length === data.length}
+              indeterminate={
+                selection.length > 0 && selection.length !== data.length
+              }
+              transitionDuration={0}
+            />
+          </th>
+          <th>Empresa</th>
+          <th>Valor</th>
+          <th>Número</th>
+          <th>Descrição</th>
+          {/* Mês de  */}
+          <th>Competencia</th>
+          {/* Data de  */}
+          <th>Recebimento</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
+  );
+}
+
+interface TableRowProps {
+  item: Invoice;
+  selection: string[];
+  toggleRow: (id: string) => void;
+}
+
+function TableRow({ item, selection, toggleRow }: TableRowProps) {
+  const { classes, cx } = useStyles();
+  const selected = selection.includes(item.id);
+
+  function openModalDelete() {}
+  function openModalMoreDetails() {}
+  function openEditForm() {}
+  return (
+    <tr key={item.id} className={cx({ [classes.rowSelected]: selected })}>
+      <td>
+        <Checkbox
+          checked={selection.includes(item.id)}
+          onChange={() => toggleRow(item.id)}
+          transitionDuration={2}
+        />
+      </td>
+      <td>
+        <Group spacing="sm">
+          <Text size="sm" weight={500}>
+            {item.corporationName}
+          </Text>
+        </Group>
+      </td>
+      <td>{item.value}</td>
+      <td>{item.number}</td>
+      <td>{getShortText(item.description, 3)}</td>
+      <td>{item.accrualMonth.toLocaleDateString()}</td>
+      <td>{item.receiptDate.toLocaleDateString()}</td>
+      <td>
+        <TableMenuRow
+          handleDelete={openModalDelete}
+          handleDetails={openModalMoreDetails}
+          handleEdit={openEditForm}
+        >
+          <ActionIcon>
+            <h4>. . .</h4>
+          </ActionIcon>
+        </TableMenuRow>
+      </td>
+    </tr>
+  );
+}
--- src/pages/historico.tsx
+++ src/pages/historico.tsx
@@ -1,4 +1,44 @@
+import { Invoice, InvoicesTable } from "components/InvoicesTable";
 import AppScheme from "layouts/AppScheme";
+
+const mockInvoices: Invoice[] = [
+  {
+    id: "o1",
+    accrualMonth: new Date(),
+    corporationName: "Empresa 1",
+    description: "Foi um gasto bastante necessario",
+    number: 12321,
+    receiptDate: new Date(),
+    value: "20",
+  },
+  {
+    id: "o2",
+    accrualMonth: new Date(),
+    corporationName: "Empresa 2",
+    description: "Foi um gasto bastante necessario",
+    number: 12321,
+    receiptDate: new Date(),
+    value: "30",
+  },
+  {
+    id: "o3",
+    accrualMonth: new Date(),
+    corporationName: "Empresa 3",
+    description: "Foi um gasto bastante necessario",
+    number: 12321,
+    receiptDate: new Date(),
+    value: "50",
+  },
+  {
+    id: "o4",
+    accrualMonth: new Date(),
+    corporationName: "Empresa 4",
+    description: "Foi um gasto bastante necessario",
+    number: 12321,
+    receiptDate: new Date(),
+    value: "133",
+  },
+];
 
 export default function IndexPage() {
   return (
@@ -7,7 +47,8 @@ export default function IndexPage() {
         <h2>Historico</h2>
 
         <h4>Tabela de Notas Fiscais Lancadas</h4>
-        <br />
+
+        <InvoicesTable data={mockInvoices} />
 
         <h4>Tabela de Despesas Lancadas</h4>
       </AppScheme>
