f2eadaf7bc9bc131c7f9b38d689752fa3e8bc040
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-19 16:01:26 +0100

    refactor: index.ts route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,8 @@
 // TODO: create all routes even no working.
 
+// TODO: create only authentication using email, and therefore create all other functions.
+// Create Facebook and Google authentication after all other functions.
+
 const hello = "Hello world";
 
 export { hello };
