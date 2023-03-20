15226f2674dd91172c00acb147071b0096debe7b
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-20 19:47:47 +0100

    refactor: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import nookies, { parseCookies } from "nookies";
-import { loginUserWithEmailPassword } from "services/loginUserWithEmailAndPassword";
 
 type MiddlewareReturn = Promise<Response | undefined> | Response | undefined;
 
