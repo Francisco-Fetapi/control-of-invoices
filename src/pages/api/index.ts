f504f579aa2592a67b42c9f7cd0f4db52fb05ffa
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-21 07:16:23 +0100

    delete: unused file

--- src/pages/api/index.ts
+++
@@ -1,8 +0,0 @@
-// TODO: create all routes even no working.
-
-// TODO: create only authentication using email, and therefore create all other functions.
-// Create Facebook and Google authentication after all other functions.
-
-const hello = "Hello world";
-
-export { hello };
