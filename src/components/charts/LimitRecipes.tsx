807b243815df42b11acfc4419a708fcc59cbbce0
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-22 05:00:29 +0100

    feat: show limit recipes

--- src/components/charts/LimitRecipes.tsx
+++ src/components/charts/LimitRecipes.tsx
@@ -3,37 +3,44 @@ import { formatMoney } from "helpers/formatMoney";
 import getPercentage from "helpers/getPercentage";
 import getSettings from "helpers/getSettings";
 import useAuth from "hooks/useAuth";
+import { TitleAndButtonActionContainer } from "styles/components/TitleAndButtonAction";
 
 export default function LimitRecipes() {
   const { user } = useAuth();
   const settings = getSettings(user);
-  const value = 23000;
+  const value = 23520;
   const total = settings.limit;
   const percentage = getPercentage({
     total,
     value,
   });
+  const remainingBilling = total - value;
 
   console.log(percentage);
   return (
     <Box>
-      <h2>Limite de Faturamento</h2>
-      <Progress
-        size="xl"
-        radius="xl"
-        sections={[
-          {
-            value: percentage,
-            color: "green",
-            label: formatMoney(value.toString()),
-          },
-          {
-            value: 100 - percentage,
-            color: "red",
-            label: formatMoney((total - value).toString()),
-          },
-        ]}
-      />
+      <TitleAndButtonActionContainer>
+        <h2>Limite de Faturamento</h2>
+        <h2>{percentage}%</h2>
+      </TitleAndButtonActionContainer>
+      <Box style={{ zoom: 1.5 }}>
+        <Progress
+          size="xl"
+          radius="xl"
+          sections={[
+            {
+              value: percentage,
+              color: "green",
+              label: formatMoney(value.toString()),
+            },
+            {
+              value: 100 - percentage,
+              color: "red",
+              label: formatMoney(remainingBilling.toString()),
+            },
+          ]}
+        />
+      </Box>
     </Box>
   );
 }
