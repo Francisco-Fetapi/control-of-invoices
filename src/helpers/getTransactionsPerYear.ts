0f7623d382526cedcb2854c463689e9126d2ece9
francisco-1678727022 <franciscofetapi10@gmail.com>
2023-03-22 07:14:12 +0100

    feat: order years of transaction

--- src/helpers/getTransactionsPerYear.ts
+++ src/helpers/getTransactionsPerYear.ts
@@ -4,11 +4,20 @@ interface Props {
   transactions?: Transaction[];
 }
 
-export default function gettransactionsYear({ transactions }: Props) {
+export default function getTransactionsYear({ transactions }: Props) {
   if (!transactions) return;
   const years = new Set<string>();
 
-  transactions.forEach((transaction) => {
+  const orderedTransaction = transactions.sort((a, b) => {
+    const dateA = new Date(a.accrualMonth);
+    const yearA = dateA.getFullYear();
+
+    const dateB = new Date(b.accrualMonth);
+    const yearB = dateB.getFullYear();
+    return yearA - yearB;
+  });
+
+  orderedTransaction.forEach((transaction) => {
     const date = new Date(transaction.accrualMonth);
     const year = date.getFullYear();
     years.add(year.toString());
